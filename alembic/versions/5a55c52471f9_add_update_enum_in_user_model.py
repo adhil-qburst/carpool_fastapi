e3757cd85725ca755c7097936350da3248fd3239
Adhil K <adhil.k@qburst.com>
2026-09-07 15:54:42 +0530

    Format the update enum in users script.

--- alembic/versions/5a55c52471f9_add_update_enum_in_user_model.py
+++ alembic/versions/5a55c52471f9_add_update_enum_in_user_model.py
@@ -5,15 +5,16 @@ Revises: f7e86b4164d5
 Create Date: 2026-09-07 15:49:35.034671
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '5a55c52471f9'
-down_revision: Union[str, Sequence[str], None] = 'f7e86b4164d5'
+revision: str = "5a55c52471f9"
+down_revision: Union[str, Sequence[str], None] = "f7e86b4164d5"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
