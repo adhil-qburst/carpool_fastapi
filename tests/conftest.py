800430dc73ff7f3ff834b7c072957b7c54ad84dc
Adhil K <adhil.k@qburst.com>
2026-09-08 12:38:44 +0530

    Format the conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 import os
-from dotenv import load_dotenv
+
 import pytest
+from dotenv import load_dotenv
 
 load_dotenv()
 
+
 @pytest.fixture(scope="session", autouse=True)
 def set_test_database_url():
-    os.environ['DATABASE_URL'] = os.getenv("TEST_DATABASE_URL")
+    os.environ["DATABASE_URL"] = os.getenv("TEST_DATABASE_URL")
