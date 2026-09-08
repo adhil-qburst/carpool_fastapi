69806b14047a756c9899e0ca47bb69c24223967e
Adhil K <adhil.k@qburst.com>
2026-09-08 12:22:01 +0530

    Add the prefic "api/v1"

--- src/app/api/router.py
+++ src/app/api/router.py
@@ -2,5 +2,5 @@ from fastapi import APIRouter
 
 from app.api import auth
 
-api_router = APIRouter()
+api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
