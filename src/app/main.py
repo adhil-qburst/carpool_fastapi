da7d2a0c367184d72ddcdd6f1acd5a375ffd1091
Adhil K <adhil.k@qburst.com>
2026-09-09 12:08:15 +0530

    Add the cors origin

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.router import api_router
 
@@ -6,6 +7,22 @@ app = FastAPI(
     title="CarPool API",
     version="0.1.0",
 )
+
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router)
 
 
